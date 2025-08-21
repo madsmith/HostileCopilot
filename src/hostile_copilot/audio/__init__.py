238706e897da8a79d87d9564a497dbc59708e194
Martin Smith <msmith@bitmonster.org>
2025-08-21 17:35:38 -0500

    Use AudioDevice for playback

--- src/hostile_copilot/audio/__init__.py
+++ src/hostile_copilot/audio/__init__.py
@@ -1,7 +1,9 @@
 from .utils import tensor_to_int16
 from .audio_data import AudioData
+from .audio_device import AudioDevice
 
 __all__ = [
     "tensor_to_int16",
     "AudioData",
+    "AudioDevice",
 ]
--- src/hostile_copilot/client/app.py
+++ src/hostile_copilot/client/app.py
@@ -1,9 +1,12 @@
 import asyncio
+import logging
 import pyaudio
-from pyaudio import PyAudio
 
 from hostile_copilot.config import OmegaConfig
 from hostile_copilot.tts.engine import TTSEngine
+from hostile_copilot.audio import AudioDevice
+
+logger = logging.getLogger(__name__)
 
 class HostileCoPilotApp:
     def __init__(self, config: OmegaConfig):
@@ -13,22 +16,32 @@ class HostileCoPilotApp:
         voices = self._config.get("tts.voices")
         self._tts_engine: TTSEngine = TTSEngine(model_id, voices)
 
+        self._audio_device: AudioDevice = AudioDevice(
+            format=pyaudio.paInt16,
+            rate=16000,
+            channels=1,
+            chunk_size=1024
+        )
+
         self._is_running: bool = False
 
 
     async def initialize(self):
+        logger.info("Initializing HostileCoPilotApp...")
+        logger.info("Initializing TTS engine...")
         await self._tts_engine.initialize()
 
+        logger.info("Initializing AudioDevice...")
+        self._audio_device.initialize()
+        self._audio_device.start()
+
     async def run(self):
+        self._is_running = True
+        
         audio = await self._tts_engine.infer("Hello, how are you?")
         print(f"Generated {len(audio)} bytes of audio.")
 
-        pa = PyAudio()
-        stream = pa.open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
-        stream.write(audio.as_bytes())
-        stream.stop_stream()
-        stream.close()
-        pa.terminate()
+        self._audio_device.play(audio)
 
         while self._is_running:
             try:
